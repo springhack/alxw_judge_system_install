b6d342bdabbaed3fb081fd234cf4f42844c17c90
springhack <springhack@live.cn>
2016-11-04 21:58:12 +0800

    Change sth

--- ubuntu16.04.sh
+++ ubuntu16.04.sh
@@ -7,7 +7,7 @@ echo 'Visit http://www.dosk/win/'
 # Install Components
 sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password password sksks'
 sudo debconf-set-selections <<< 'mysql-server-5.7 mysql-server/root_password_again password sksks'
-sudo apt -y install mysql-server-5.7 apache2 php php-curl php-mbstring git python-dev python-mysqldb libapache2-mod-php build-essential openjdk-8-jdk
+sudo apt -y install mysql-server-5.7 apache2 php php-curl php-mbstring git python-dev python-mysqldb python-pycurl libapache2-mod-php build-essential openjdk-8-jdk
 
 # Clone Code
 git clone https://github.com/springhack/alxw_judge_system_v1
@@ -19,28 +19,34 @@ cd Lo-runner
 sudo ./setup.py install
 cd ..
 
-mkdir -p /var/www/html/alxwvj
+sudo mkdir -p /var/www/html/alxwvj
 
 cd alxw_judge_system_v1
-cp -rvf ./* /var/www/html/alxwvj/
+sudo cp -rvf ./* /var/www/html/alxwvj/
 cd ..
 
 cd alxw_judge_core
 sudo ./Install.sh install
 cd ..
 
-cp /var/www/html/alxwvj/classes/alxwd /etc/init.d/alxwd
+sudo cp /var/www/html/alxwvj/classes/alxwd /etc/init.d/alxwd
 
 # Change Permission
 sudo chown -R www-data:www-data /var/www/html/alxwvj
 sudo chmod 755 /etc/init.d/alxwd
 sudo chmod 755 /etc/init.d/ajcd
 
+# Initial MySQL
+mysql -uroot -psksks << EOF
+create database build_vj;
+EOF
+
 # Change Config
 sudo php ./makeconfig.php
-a2enmod rewrite
+sudo a2enmod rewrite
 echo 'Change apache config'
 sudo sed -i 's/AllowOverride\ None/AllowOverride\ All/g' /etc/apache2/apache2.conf
+sudo sed -i 's/\/home\/wwwroot\/default\/classes\/Server.php/\/var\/www\/html\/alxwvj\/classes\/Server.php/g' /etc/init.d/alxwd
 
 sudo /etc/init.d/alxwd start
 sudo /etc/init.d/ajcd start
